be0e1767258bbec903d9da88beca001e17da3b9d
hyoilll <50327128+hyoilll@users.noreply.github.com>
2021-01-23 11:42:04 +0900

    server to client transmission / file-list print end

--- file-transmission/server.py
+++ file-transmission/server.py
@@ -1,6 +1,7 @@
 from socket import *
-from os.path import exists
+from os.path import exists, dirname
 import sys
+import os
 
 serverSock = socket(AF_INET, SOCK_STREAM)
 
@@ -15,25 +16,43 @@ connectionSock, addr = serverSock.accept()
 
 print(str(addr), ' 에서 접속하였습니다.')
 
-# 클라이언트에게 파일이름 (바이트형식) 으로 전달받음
-fileName = connectionSock.recv(1024)
-print('받은 데이터 : ', fileName.decode('utf-8'))
+while True:
+    # menu 전달받음
+    print('menu 수신 대기중')
+    menu = int(connectionSock.recv(1024).decode('utf-8'))
 
-data_transferred = 0
+    if menu == 1:  # 1 클라이언트 -> 서버 업로드
+        pass
+    elif menu == 2:  # 2 서버 -> 클라이언트 다운로드
+        # 클라이언트에게 파일이름 (바이트형식) 으로 전달받음
+        print('2')
+        fileName = connectionSock.recv(1024)
+        print('받은 데이터 : ', fileName.decode('utf-8'))
 
-if not exists(fileName):
-    print('no file')
-    sys.exit()
+        data_transferred = 0
 
-print('파일 : %s / 전송 시작' % fileName)
+        if not exists(fileName):
+            print('no file')
+            sys.exit()
 
-with open(fileName, 'rb') as f:
-    try:
-        data = f.read(1024)  # 1024바이트 읽음
-        while data:  # data가 없을 때 까지
-            data_transferred += connectionSock.send(data)
-            data = f.read(1024)
-    except Exception as ex:
-        print(ex)
+        print('파일 : %s / 전송 시작' % fileName)
 
-print('전송완료 %s, 전송량 %d' % (fileName, data_transferred))
+        with open(fileName, 'rb') as f:
+            try:
+                data = f.read(1024)  # 1024바이트 읽음
+                while data:  # data가 없을 때 까지
+                    data_transferred += connectionSock.send(
+                        data)
+                    data = f.read(1024)
+                    print(len(data))
+            except Exception as ex:
+                print(ex)
+
+        print('전송완료 %s, 전송량 %d' % (fileName, data_transferred))
+    else:  # 3 서버 파일 리스트 조회
+        path_dir = 'C:\\Users\\dlgyd\\OneDrive\\문서\\GitHub\\python-socket-server\\file-transmission'
+        file_list = os.listdir(path_dir)
+        file_list = ', '.join(file_list)
+        print(file_list)
+
+        connectionSock.send(file_list.encode('utf-8'))
